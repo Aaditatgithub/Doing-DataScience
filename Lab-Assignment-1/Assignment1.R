eb82a47ed9d55d080a8c857e9872d81b60866846
Aaditya Patil <131251920+Aaditatgithub@users.noreply.github.com>
2024-01-03 14:38:39 +0530

    First commit

---
+++ Lab-Assignment-1/Assignment1.R
@@ -0,0 +1,36 @@
+c <- c(10,543,652342,2134,1124)
+c
+c = c+1
+c
+c++
+c
+a <- c[c(1,2,3)]
+a
+c[-2]
+c
+c>50
+which(c>50)
+c[which(c>50)]
+f <- c[which(c > 50 & c < 90)]
+f
+
+c[2] = 10
+c
+sort(c,decreasing = TRUE)
+c <- sort(c)
+c
+sum(c)
+min(c)
+max(c)
+length(c)
+
+vec1 <- seq(3,20,4)    #Start, end, update
+rep(vec1,2)
+
+typeof(8L)
+class(8L)
+class(TRUE)
+class('C')
+class("Hello world")
+c <- 3 + 4i
+typeof(c)
